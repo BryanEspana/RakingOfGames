88a9bcf9655f7c1023dbc562b6ed212dacb0d2ee
Bryan España <bryanespana21550@gmail.com>
2024-02-12 23:24:47 -0600

    Pagina principal completa

--- src/app/pages/Carrousel/Carrousel.component.ts
+++ src/app/pages/Carrousel/Carrousel.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+
+
+export interface CarouselItem {
+  title: string;
+  category: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-Carrousel',
@@ -6,10 +13,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Carrousel.component.scss']
 })
 export class CarrouselComponent implements OnInit {
+  
+  @Input() items: CarouselItem[] = [];
+  @ViewChild('carousel') carousel?: ElementRef<HTMLDivElement>;
 
   constructor() { }
 
   ngOnInit() {
   }
+  scrollLeft() {
+    this.carousel?.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
+  }
 
+  scrollRight() {
+    this.carousel?.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+  }
+  
 }
--- src/app/pages/Initial/Initial.component.ts
+++ src/app/pages/Initial/Initial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselItem } from '../Carrousel/Carrousel.component';
 
 @Component({
   selector: 'app-Initial',
@@ -6,7 +7,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Initial.component.scss']
 })
 export class InitialComponent implements OnInit {
-
+  listaItems: CarouselItem[] = [
+    {
+      title: 'Palworld',
+      category: 'Aventura',
+      image: '../assets/Games/palworld-1705691572614.jpg'
+    },
+    {
+      title: 'Item 2',
+      category: 'Category 2',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Item 3',
+      category: 'Category 3',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Item 4',
+      category: 'Category 4',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Item 5',
+      category: 'Category 5',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Item 6',
+      category: 'Category 6',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Item 3',
+      category: 'Category 3',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Item 4',
+      category: 'Category 4',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Item 5',
+      category: 'Category 5',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Item 6',
+      category: 'Category 6',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Item 3',
+      category: 'Category 3',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Item 4',
+      category: 'Category 4',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Item 5',
+      category: 'Category 5',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Item 6',
+      category: 'Category 6',
+      image: 'https://via.placeholder.com/150'
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
