d02895519e1a3ef1b920b3ae5e87e41d754cc747
Bryan España <bryanespana21550@gmail.com>
2024-02-13 00:09:52 -0600

    Primeras implementaciones

--- src/app/config/backEndRoutes.ts
+++ src/app/config/backEndRoutes.ts
@@ -1,3 +1,5 @@
 
 //top-games
 export const TOP_GAMES_URL = '/top-games';
+//Mejores CATEGORIAS segun metacritica
+export const TOP_GAMES_METACRITIC_URL = 'top-genres-metacritic';
--- src/services/games.service.ts
+++ src/services/games.service.ts
@@ -1,10 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, catchError, throwError } from 'rxjs';
+import { TOP_GAMES_URL } from 'src/app/config/backEndRoutes';
+import { API_URL } from 'src/app/config/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GamesService {
 
-constructor() { }
+constructor(
+  public http: HttpClient,
+  ) { }
+
+
+  public ObtenerTopGames() {
+    const url = `${API_URL}${TOP_GAMES_URL}`;
+    return this.http.get<any[]>(url, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    }).pipe(
+      map(response => response.map(game => ({
+        name: game.name,
+        genre: game.genres.length > 0 ? game.genres[0].name : 'Unknown', // Asume el primer género si existe, de lo contrario 'Unknown'
+        image: game.background_image,
+        rating: game.rating
+      }))),
+      catchError(err => {
+        console.error("ERROR al obtener los juegos:", err);
+        return throwError(() => err);
+      })
+    );
+  }
 
 }
